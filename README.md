08489a8f2787b94e299dbbc9bdd25999ebd6804f
Charles Karafotias <cnkara2023@gmail.com>
2025-03-23 11:27:30 -0400

    5 - parser complete

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ _DISCLAIMER: Tap is a work in progress and is under active development. Not all 
   - If you’re already inside a stored repository directory, you don’t need to specify the repo name. Instead, tap provides the `here` keyword to autofill this context, for example `tap here secret` will retrieve the associated secret link stored under the current repository, if it exists.
   - *TODO* add in file tree and table showing what is stored to make the provided example more clear
 - **Flexible Use Cases**
-  - Originally designed for Software Engineers with way too many repositories, Tap specializes in quickly accessing build artifacts, logs, and pipeline dashboards without searching for links. However, Tap is not limited to this one usecase. Tap's design adapts well to both personal and enterprise use cases where directories & files on a local computer have couterparts online.
+  - Originally designed for Software Engineers with way too many repositories, Tap specializes in quickly accessing build artifacts, logs, and pipeline dashboards without searching for links. However, Tap is not limited to this one use case. Tap's design adapts well to both personal and enterprise use cases where directories & files on a local computer have counterparts online.
   - Examples:
     - Software Engineers
       - Did my pushed code just succeed or fail? Let's find out with `tap here build`
@@ -28,14 +28,14 @@ _DISCLAIMER: Tap is a work in progress and is under active development. Not all 
   - Easily add, update, remove, or list your parent entities and associated links via our CLI command `tap -l` or `tap --list`
   - Prefer a UI... Tap has an interactive terminal user interface as well `tap --tui` 
 - **Auto-Completion**
-  - What CLI doesn't have auto complete these days? With Tap, the goal is to get you where you want to go fast. Tap dynamically generates auto-complete suggestions whenever you hit tab, its that simple!
+  - What CLI doesn't have auto complete these days? With Tap, the goal is to get you where you want to go fast. Tap dynamically generates auto-complete suggestions whenever you hit tab, it's that simple!
   - Say you are typing `tap my-`
-    - Tap will look through your parent entities and see if there's any matches. If there,s multiple, Tap lists them out to help you narrow down your search. Otherwise, it auto-fills to move you along in your command
+    - Tap will look through your parent entities and see if there's any matches. If there,s multiple, Tap lists them out to help you narrow down your search. Otherwise, it will autofill to move you along in your command
   - Of course Tap also supports this for links within your parent entities. For example, say you type `tap my-repository secre` and `my-repository` only has one link named `secrets`. Hitting tab, Tap will auto complete the typing of `secrets` for you.
 - **Easy Onboarding Via Bulk Import**
   - Bookmark managers have been around for years. Knowing this, Tap allows imports of the following browsers' bookmark manager files
     - Chrome, Edge, Firefox, Opera, Safari
-  - So your a programmer that wants to generate your own file of links into Tap? That's awesome, and also supported using YAML syntax. Below makes one new parent-entity called `tmgr` with a `repository` link (feel free to check out `tmgr` if your looking to manage tasks using a CLI!)
+  - So you're a programmer that wants to generate your own file of links into Tap? That's awesome, and also supported using YAML syntax. Below makes one new parent-entity called `tmgr` with a `repository` link (feel free to check out `tmgr` if your looking to manage tasks using a CLI!)
     ```yaml
     - tmgr:
         repository: https://github.com/CharlieKarafotias/tmgr/tree/main
@@ -45,22 +45,38 @@ _DISCLAIMER: Tap is a work in progress and is under active development. Not all 
   - Tap supports migrating to the following browsers' bookmark managers:
     - Chrome, Edge, Firefox, Opera, Safari 
 - **Multi-OS Support**
-  - Tap is offered on MacOS and UNIX platforms at this time.
+  - Tap is offered on macOS and UNIX platforms at this time.
   - In the future, Windows will also be supported. 
 - **Built-in Updater**
   - You will always stay up to date with the built in `tap update` command.
  
 ## Reserved Keyword
 
-Given the features provided by `tap` out of the box, some key words must be reserved. 
+Given the features provided by `tap` out of the box, some keywords must be reserved. 
 
 Reserved List:
 - `here`
-- Parent entities can't start with `-`. This is because all `tap` commands like `list` or `update` start with `-` or `--` to distinguish them
+- Parent entities can not be the following keywords:
+  - `-a`
+  - `--add`
+  - `-d`
+  - `--delete`
+  - `--export`
+  - `--help`
+  - `-i`
+  - `--init`
+  - `--import`
+  - `-s`
+  - `--show`
+  - `-u`
+  - `--update`
+  - `--upsert`
+  - `-v`
+  - `--version`
 
 ### How Does This Affect Me?
 
-It most likely won't! The only way reserved words will affect you is if you attempt to create a new parent entity using one of the reserved key words. In the case you accidently do, an error will be raised, allowing you to edit your name.
+It most likely won't! The only way reserved words will affect you is if you attempt to create a new parent entity using one of the reserved keywords. In the case you accidentally do, an error will be raised, allowing you to edit your name.
 
 ## Developer Documentation
 
--- src/cli.rs
+++ src/cli.rs
@@ -11,6 +11,121 @@ pub fn run() {
     // match by len
     let res = match args.len() {
         0 => display_help(),
+        1 => {
+            match args[0].as_str() {
+                // General:
+                "--help" => display_help(),
+                "-v" | "--version" => display_version(),
+                // Utilities:
+                "--update" => "TODO: Implement update functionality".to_string(),
+                "-i" | "--init" => "TODO: Implement init functionality".to_string(),
+                // Opening links:
+                "here" => "TODO: Implement here functionality".to_string(),
+                parent_entity => format!(
+                    "TODO: Implement open functionality for Parent Entity: {}",
+                    parent_entity
+                ),
+            }
+        }
+        2 => {
+            match args[0].as_str() {
+                // Utilities
+                "--import" => match args[1].as_str() {
+                    "Chrome" => "TODO: Implement import functionality from Chrome".to_string(),
+                    "Edge" => "TODO: Implement import functionality from Edge".to_string(),
+                    "Firefox" => "TODO: Implement import functionality from Firefox".to_string(),
+                    "Opera" => "TODO: Implement import functionality from Opera".to_string(),
+                    "Safari" => "TODO: Implement import functionality from Safari".to_string(),
+                    "Tap" => "TODO: Implement import functionality from Tap".to_string(),
+                    _ => display_error(),
+                },
+                "--export" => match args[1].as_str() {
+                    "Chrome" => "TODO: Implement export functionality to Chrome".to_string(),
+                    "Edge" => "TODO: Implement export functionality to Edge".to_string(),
+                    "Firefox" => "TODO: Implement export functionality to Firefox".to_string(),
+                    "Opera" => "TODO: Implement export functionality to Opera".to_string(),
+                    "Safari" => "TODO: Implement export functionality to Safari".to_string(),
+                    "Tap" => "TODO: Implement export functionality to Tap".to_string(),
+                    _ => display_error(),
+                },
+                // Adding, Updating, and Deleting links:
+                "-d" | "--delete" => match args[1].as_str() {
+                    "here" => "TODO: Implement delete functionality for here".to_string(),
+                    parent_entity => format!(
+                        "TODO: Implement delete functionality for Parent Entity: {}",
+                        parent_entity
+                    ),
+                },
+                "-s" | "--show" => match args[1].as_str() {
+                    "here" => "TODO: Implement show functionality for here".to_string(),
+                    parent_entity => format!(
+                        "TODO: Implement show functionality for Parent Entity: {}",
+                        parent_entity
+                    ),
+                },
+                // Opening links:
+                "here" => format!(
+                    "TODO: Implement open functionality for here with Link Name {}",
+                    args[1]
+                ),
+                parent_entity => format!(
+                    "TODO: Implement open functionality for Parent Entity {} with Link Name {}",
+                    parent_entity, args[1]
+                ),
+            }
+        }
+        3 => {
+            match args[0].as_str() {
+                // Adding, Updating, and Deleting links:
+                "-d" | "--delete" => match args[1].as_str() {
+                    "here" => format!(
+                        "TODO: Implement delete functionality for here with Link Name {}",
+                        args[2]
+                    ),
+                    parent_entity => format!(
+                        "TODO: Implement delete functionality for Parent Entity {} with Link Name {}",
+                        parent_entity, args[2]
+                    ),
+                },
+                "-s" | "--show" => match args[1].as_str() {
+                    "here" => format!(
+                        "TODO: Implement show functionality for here with Link Name {}",
+                        args[2]
+                    ),
+                    parent_entity => format!(
+                        "TODO: Implement show functionality for Parent Entity {} with Link Name {}",
+                        parent_entity, args[2]
+                    ),
+                },
+                _ => display_error(),
+            }
+        }
+        4 => {
+            match args[0].as_str() {
+                // Adding, Updating, and Deleting links:
+                "-a" | "--add" => match args[1].as_str() {
+                    "here" => format!(
+                        "TODO: Implement add functionality for here with Link Name {} and Value {}",
+                        args[2], args[3]
+                    ),
+                    parent_entity => format!(
+                        "TODO: Implement add functionality for Parent Entity {} with Link Name {} and Value {}",
+                        parent_entity, args[2], args[3]
+                    ),
+                },
+                "-u" | "--upsert" => match args[1].as_str() {
+                    "here" => format!(
+                        "TODO: Implement upsert functionality for here with Link Name {} and Value {}",
+                        args[2], args[3]
+                    ),
+                    parent_entity => format!(
+                        "TODO: Implement upsert functionality for Parent Entity {} with Link Name {} and Value {}",
+                        parent_entity, args[2], args[3]
+                    ),
+                },
+                _ => display_error(),
+            }
+        }
         _ => display_error(),
     };
 
@@ -39,20 +154,23 @@ fn display_commands() -> String {
     let mut s = "".to_string();
     s.push_str("Usage:\n");
     s.push_str("  Opening Links:\n");
-    s.push_str("   - tap <Parent Entity> [Link Name]                         Open all Parent Entity's Link. When Link Name is supplied, opens the specific Link\n");
-    s.push_str("   - tap here [Link Name]                                    Uses the current directory as the Parent Entity. Opens all Parent Entity's links. When Link Name is supplied, opens the specific Link\n");
+    s.push_str("   - tap <Parent Entity> [Link Name]                                   Open all Parent Entity's Link. When Link Name is supplied, opens the specific Link\n");
+    s.push_str("   - tap here            [Link Name]                                   Uses the current directory as the Parent Entity. Opens all Parent Entity's links. When Link Name is supplied, opens the specific Link\n");
     s.push_str("  Adding, Updating, and Deleting Links:\n");
-    s.push_str("   - tap (-a, --add) <Parent Entity> <Link Name> <Value>     Add a new Link to the Parent Entity. It will create the Parent Entity if it doesn't exist\n");
-    s.push_str("   - tap (-d, --delete) <Parent Entity> [Link Name]          Deletes an existing Link from the Parent Entity. If no Link Name is provided, deletes all Links from the Parent Entity\n");
-    s.push_str("   - tap (-s, --show) <Parent Entity> [Link Name]            Shows the value of an existing Link from the Parent Entity. If no Link Name is provided, shows all Link Values from the Parent Entity\n");
-    s.push_str("   - tap (-u, --upsert) <Parent Entity> <Link Name> <Value>  Upsert an existing Link in the Parent Entity. It will create the Link and Parent Entity if it doesn't exist\n");
+    s.push_str("   - tap (-a, --add)    <Parent Entity | here> <Link Name> <Value>     Add a new Link to the Parent Entity. It will create the Parent Entity if it doesn't exist\n");
+    s.push_str("   - tap (-d, --delete) <Parent Entity | here> [Link Name]             Deletes an existing Link from the Parent Entity. If no Link Name is provided, deletes all Links from the Parent Entity\n");
+    s.push_str("   - tap (-s, --show)   <Parent Entity | here> [Link Name]             Shows the value of an existing Link from the Parent Entity. If no Link Name is provided, shows all Link Values from the Parent Entity\n");
+    s.push_str("   - tap (-u, --upsert) <Parent Entity | here> <Link Name> <Value>     Upsert an existing Link in the Parent Entity. It will create the Link and Parent Entity if it doesn't exist\n");
     s.push_str("  Utilities:\n");
-    s.push_str("   - tap (-i, --init)                                        Initializes Tap (Shell Auto-Completion, etc.)\n");
-    s.push_str("   - tap --import <Browser | Tap>                            Imports a bookmark file into Tap. Supports both Tap Files and the following browsers' bookmark manager files: Chrome, Edge, Firefox, Opera, Safari\n");
-    s.push_str("   - tap --export <Browser | Tap>                            Exports Tap to a bookmark file. Supported Browsers: Chrome, Edge, Firefox, Opera, Safari\n");
-    s.push_str("   - tap --update                                            Updates Tap\n");
+    s.push_str("   - tap (-i, --init)                                                  Initializes Tap (Shell Auto-Completion, etc.)\n");
+    s.push_str("   - tap --import     <Browser | Tap>                                  Imports a bookmark file into Tap. Supports both Tap Files and the following browsers' bookmark manager files: Chrome, Edge, Firefox, Opera, Safari\n");
+    s.push_str("   - tap --export     <Browser | Tap>                                  Exports Tap to a bookmark file. Supported Browsers: Chrome, Edge, Firefox, Opera, Safari\n");
     s.push_str(
-        " - tap --help                                                Display this help message\n",
+        "   - tap --update                                                      Updates Tap\n",
     );
+    s.push_str(" - tap --help                                                          Display this help message\n");
+    s.push_str(" - tap (-v, --version)                                                 Display the version\n");
     s
 }
+
+// TODO: add tests for all functions and the run function
