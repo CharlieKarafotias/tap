ae5855c02971f30e43ac29b88f56539900e5e26b
Charles Karafotias <cnkara2023@gmail.com>
2025-05-24 07:31:33 -0400

    16 - conditional for selected_parent works

--- src/commands/init/shell_completions.rs
+++ src/commands/init/shell_completions.rs
@@ -7,30 +7,7 @@ pub(super) const ZSH_COMPLETION: &str = r#"#compdef tap
 local -a parents
 parents=("${(@f)$(tap -s | tail -n +2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e '/^$/d')}")
 
-# TODO: fetch dynamically (need to update so commands without groups for ex: export do not wrap in {}. Also need to figure out why the commands arent parsing correctly in ZSH)
-# commands=("${(@f)$(
-#   tap --help |
-#   sed -n '/^Commands:/,$p' |
-#   sed '1d' |
-#   grep '^[[:space:]]*-' |
-#   awk '{
-#     sub(/^[[:space:]]*/, "", $0);
-#     split($0, parts, /[[:space:]]{2,}/);
-#     opts_raw = parts[1];
-#     if (length(parts) > 1 && parts[length(parts)] == "") {
-#         desc = parts[length(parts) - 1];
-#     } else {
-#         desc = parts[length(parts)];
-#     }
-#     sub(/, /, ",", opts_raw);
-#     opts_spaced = opts_raw;
-#     sub(/,/, " ", opts_spaced);
-#     q="\047";
-#     printf "%s(%s)%s{%s}%s[%s]%s\n", q, opts_spaced, q, opts_raw, q, desc, q;
-#   }'
-# )}")
-
-# BELOW is temporary while i figure out how to dynamically fetch commands
+# TODO: fetch dynamically using --help command (temp workaround)
 local -a commands
 commands=(
   '(-a --add)'{-a,--add}'[Add a new link]'
@@ -44,6 +21,28 @@ commands=(
   '(--update)'--update'[Update Tap to the latest version]'
   '(--help)'--help'[Display this help message]'
   '(-v --version)'{-v,--version}'[Show tap version]'
+)
+
+# Extract just the command names (without the options) for comparison
+local -a command_options
+command_options=(
+  '-a'
+  '--add'
+  '-d'
+  '--delete'
+  '-s'
+  '--show'
+  '-u'
+  '--upsert'
+  '-i'
+  '--init'
+  '--import'
+  '--export'
+  '--tui'
+  '--update'
+  '--help'
+  '-v'
+  '--version'
 )
 
 _arguments \
@@ -60,11 +59,25 @@ case $state in
         fi
     ;;
     command)
-        # TODO: add check for if parent entity or commands
         local selected_parent=$words[2]
-        local selected_links
-        selected_links=($(tap -s $selected_parent | tail -n +2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e '/^$/d'))
-        _values 'Links' $selected_links
+        
+        # Check if the selected word is a command or a parent entity
+        if (($command_options[(Ie)$selected_parent])); then
+            # If it's a command option, provide command-specific completions
+            case $selected_parent in
+                -a|--add)
+                    _values 'Add options' "parent" "link" "value"
+                    ;;
+                -d|--delete)
+                    _values 'Delete options' "parent" "link"
+                    ;;
+            esac
+        else
+            print "DEBUG: Selected parent entity: $selected_parent"
+            local selected_links
+            selected_links=($(tap -s $selected_parent | tail -n +2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e '/^$/d'))
+            _values 'Links' $selected_links
+        fi
     ;;
 esac
 "#;
